778e8cce4bd3984d95ce7120be30a4d2edd8a8fe
Melih Çelik <57464067+yenilikci@users.noreply.github.com>
2021-01-22 23:35:12 +0300

    .htaccess Ayarları

--- application/controllers/Member.php
+++ application/controllers/Member.php
@@ -4,17 +4,17 @@ class Member extends CI_Controller{
 	
 	public function index()
 	{
-
+		echo "test";
 	}
 
 	public function registration()
 	{
-
+		echo "register";
 	}
 
 	public function activation()
 	{
-		
+
 	}
 
 }
